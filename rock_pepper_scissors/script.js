ec3f63adb3970b53a0a9cbfbc3c27ce3e41609ae
mahbubshaun <mdmahbubshaun@gmail.com>
2024-07-02 00:05:36 +0600

    rock pepper scissor project completed

--- rock_pepper_scissors/script.js
+++ rock_pepper_scissors/script.js
@@ -1,30 +1,72 @@
-function getComputerChoice()
-{
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    // your code here!
+    if (humanChoice === computerChoice)
+    {
+        humanScore++;
+        computerScore--;
+    }
+    else{
+        humanScore--;
+        computerScore++;
+    }
+
+    console.log(`So far the human score is ${humanScore}`);
+    console.log(`So far the computer score is ${computerScore}`);
+}
+
+
+
+function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
-if (computerChoice === 0 )
-{
-    console.log('Computer selected Rock')
-}
+// let computerChoice = getComputerChoice();
+// console.log(computerChoice);
+// if (computerChoice === 0) {
+//     console.log('Computer selected Rock')
+// }
 
-else if (computerChoice === 1 )
-{
-    console.log('Computer selected Pepper')
-}
+// else if (computerChoice === 1) {
+//     console.log('Computer selected Pepper')
+// }
 
-else
-{
-    console.log('Computer selected Scissors')
-}
+// else {
+//     console.log('Computer selected Scissors')
+// }
 
 
 
-function getHumanChoice()
-{
+function getHumanChoice() {
     return parseInt(prompt(' Please select a value for the game '));
 
 }
 
 console.log(getHumanChoice());
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+
+    }
+
+    if (humanScore > computerScore)
+    {
+        console.log('Human wins!');
+    }
+    else{
+        console.log('Computer wins!');
+    }
+}
+
+playGame()
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
