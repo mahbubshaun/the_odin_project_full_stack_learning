cba5126f7dfc57a617f8f18f843b99d6af680836
unknown <mdmahbubshaun@gmail.com>
2024-07-06 23:31:27 +0600

    dom manipulation assignments

--- dom_manipulation/js-file.js
+++ dom_manipulation/js-file.js
@@ -37,3 +37,37 @@ div.appendChild(headerInsideDiv);
 div.appendChild(paragraphInsideDiv);
 
 container.appendChild(div);
+
+
+// the JavaScript file
+// const btn = document.querySelector("#btn");
+// btn.onclick = () => alert("Hello World");
+
+
+// the JavaScript file
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  alert("Hello World");
+});
+
+btn.addEventListener("click", function (e) {
+    console.log(e);
+  });
+
+  btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+  });
+  
+
+  // buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+  
