1e3365c1bb010cf779742330e01c4117cd2a026b
unknown <mdmahbubshaun@gmail.com>
2024-07-08 15:45:02 +0600

    rock-paper-scissor logic update

--- rock_pepper_scissors/script.js
+++ rock_pepper_scissors/script.js
@@ -4,15 +4,85 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     // your code here!
-    if (humanChoice === computerChoice)
-    {
-        humanScore++;
-        computerScore--;
+
+    let humanAnswer = humanChoice.toLowerCase();
+
+    switch (humanAnswer) {
+        case 'rock':
+            console.log('Human choosed \'rock\'');
+            if (computerChoice === 0) {
+                console.log('Both human & computer choosed same value');
+            }
+
+
+            if (computerChoice === 1) {
+                console.log('Computer choosed Paper');
+                console.log('Paper beats rock!, Computer Wins!');
+                computerScore++;
+            }
+
+            if (computerChoice === 2) {
+                console.log('Computer choosed scissor');
+                console.log('Rock beats scissor!, Human Wins!');
+                humanScore++;
+            }
+            break;
+
+        case 'paper':
+            console.log('Human choosed \'paper\'');
+            if (computerChoice === 0) {
+                console.log('Computer choosed Rock');
+                console.log('Paper beats rock!, Human Wins!');
+                humanScore++;
+            }
+
+
+            if (computerChoice === 1) {
+                console.log('Computer choosed paper');
+                console.log('Both human & computer choosed same value');
+            
+            }
+
+            if (computerChoice === 2) {
+                console.log('Computer choosed scissor');
+                console.log('Scissor beats paper!, Computer Wins!');
+                computerScore++;
+            }
+            break;
+
+        case 'scissor':
+            console.log('Human choosed \'scissor\'');
+            if (computerChoice === 0) {
+                console.log('Computer choosed rock');
+                console.log('Rock beats scissor, Computer wins!');
+                computerScore++;
+            }
+
+
+            if (computerChoice === 1) {
+                console.log('Computer choosed Paper');
+                console.log('Scissor beats paper!, Human Wins!');
+                humanScore++;
+            }
+
+            if (computerChoice === 2) {
+                console.log('Computer choosed scissor');
+                console.log('Both human & computer choosed same value');
+            }
+            break
+
+        default:
+            break;
     }
-    else{
-        humanScore--;
-        computerScore++;
-    }
+
+    // if (humanChoice === computerChoice) {
+    //     humanScore++;
+    //     computerScore--;
+    // }
+    // else {
+    //     humanScore--;
+    //     computerScore++;
+    // }
 
     console.log(`So far the human score is ${humanScore}`);
     console.log(`So far the computer score is ${computerScore}`);
@@ -40,7 +110,7 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-    return parseInt(prompt(' Please select a value for the game '));
+    return prompt(' Please select a value for the game');
 
 }
 
@@ -49,18 +119,18 @@ console.log(getHumanChoice());
 function playGame() {
     for (let i = 0; i < 5; i++) {
         let humanSelection = getHumanChoice();
+
         let computerSelection = getComputerChoice();
 
-        playRound(humanSelection, computerSelection);
+        playRound(humanSelection.toLowerCase(), computerSelection);
 
 
     }
 
-    if (humanScore > computerScore)
-    {
+    if (humanScore > computerScore) {
         console.log('Human wins!');
     }
-    else{
+    else {
         console.log('Computer wins!');
     }
 }
