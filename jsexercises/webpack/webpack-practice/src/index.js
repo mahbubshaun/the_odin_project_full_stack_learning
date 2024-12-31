7a0aee04fe2f8b82f34666b48a7fdca79b5428a4
mahbubshaun <mdmahbubshaun@gmail.com>
2024-12-31 23:53:27 +0600

    oop

--- jsexercises/webpack/webpack-practice/src/index.js
+++ jsexercises/webpack/webpack-practice/src/index.js
@@ -1,4 +1,24 @@
 import "./styles.css";
+import { cube } from './math.js';
 import { greeting } from "./greetings.js";
 
 console.log(greeting);
+
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+
+
+function component() {
+    const element = document.createElement('pre');
+
+    element.innerHTML = [
+      'Hello webpack!',
+      '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
