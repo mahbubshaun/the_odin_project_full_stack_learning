1acaa0ccd3aabe71202507d41eba7bfb2f7a113d
mahbubshaun <mdmahbubshaun@gmail.com>
2025-03-22 02:11:56 +0600

    update library project using class

--- LibraryModified/script_using_class.js
+++ LibraryModified/script_using_class.js
@@ -7,8 +7,8 @@ class Book {
       this.source = source;
     }
   
-    info() {
-      return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    toggleRead() {
+      this.read = this.read === "Read" ? "Unread" : "Read";
     }
   }
   
@@ -17,52 +17,52 @@ class Book {
   function addBookToArray(title, author, pages, read, source) {
     const book = new Book(title, author, pages, read, source);
     myLibrary.push(book);
-    console.log("Book added successfully");
-    let id = myLibrary.length - 1;
-    createBookCardElements(book, id);
+    renderBooks();
   }
   
-  function createBookCardElements(book, id) {
+  function renderBooks() {
     const bookContainer = document.querySelector(".book-container");
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    bookCard.id = id;
+    bookContainer.innerHTML = ""; // Clear existing books before re-rendering
   
-    bookCard.innerHTML = `
-      <div class="img-container">
-        <img class="book-cover" src="${book.source}" alt="Book Cover">
-      </div>
-      <div class="details">
-        <div class="book-title-con">
-          <h1 class="book-title">${book.title}</h1>
-        </div>
-        <div class="author">
-          <h3 class="book-author">${book.author}</h3>
-        </div>
-        <div class="pages-con">
-          <p class="pages">${book.pages}</p>
-        </div>
-        <div class="read">
-          <button class="read-button">${book.read}</button>
-          <button class="delete-button">Delete</button>
-        </div>
-      </div>
-    `;
+    myLibrary.forEach((book, index) => {
+      const bookCard = document.createElement("div");
+      bookCard.classList.add("book-card");
+      bookCard.dataset.id = index;
   
-    bookContainer.appendChild(bookCard);
+      bookCard.innerHTML = `
+        <div class="img-container">
+          <img class="book-cover" src="${book.source}" alt="Book Cover">
+        </div>
+        <div class="details">
+          <div class="book-title-con">
+            <h1 class="book-title">${book.title}</h1>
+          </div>
+          <div class="author">
+            <h3 class="book-author">${book.author}</h3>
+          </div>
+          <div class="pages-con">
+            <p class="pages">${book.pages}</p>
+          </div>
+          <div class="read">
+            <button class="read-button">${book.read}</button>
+            <button class="delete-button">Delete</button>
+          </div>
+        </div>
+      `;
   
-    const readButton = bookCard.querySelector(".read-button");
-    readButton.addEventListener("click", () => {
-      book.read = book.read === "Read" ? "Unread" : "Read";
-      readButton.textContent = book.read;
-      console.log(myLibrary);
-    });
+      bookContainer.appendChild(bookCard);
   
-    const deleteButton = bookCard.querySelector(".delete-button");
-    deleteButton.addEventListener("click", () => {
-      bookContainer.removeChild(bookCard);
-      myLibrary.splice(id, 1);
-      console.log(myLibrary);
+      const readButton = bookCard.querySelector(".read-button");
+      readButton.addEventListener("click", () => {
+        book.toggleRead();
+        renderBooks();
+      });
+  
+      const deleteButton = bookCard.querySelector(".delete-button");
+      deleteButton.addEventListener("click", () => {
+        myLibrary.splice(index, 1);
+        renderBooks();
+      });
     });
   }
   
